ad5b6181bb5fb8d0444b7a4a7fa13e1bb131191c
Diogo Coelho <dcarvalh@student.42lisboa.com>
2022-12-13 19:55:25 +0000

    Ready to start

--- front-end/comp/MainPage/Header/Btns/Profile/Profile.js
+++ front-end/comp/MainPage/Header/Btns/Profile/Profile.js
@@ -1,21 +1,26 @@
-import Dropdown from "react-bootstrap/Dropdown";
-import { BsPersonFill }  from "react-icons/bs"
-import styles from "./Profile.module.scss"
-const Profile = () => {
-  return (
-    <Dropdown >
-      <Dropdown.Toggle variant="success" id="dropdown-basic" color="blue">
-        <BsPersonFill/>
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">View Profile</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Orders</Dropdown.Item>
-        <Dropdown.Divider/>
-        <Dropdown.Item href="#/action-3">Log Out</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
+import Dropdown from 'react-bootstrap/Dropdown';
+import { BsPersonFill } from "react-icons/bs"
+import { useRouter } from 'next/router';
+const Profile = () =>{
+    const router = useRouter()
+    const viewHandler = () =>{
+        router.push({
+            pathname: "/profile"
+        })
+    }
+    return(
+        <Dropdown >
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+          <BsPersonFill/>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={viewHandler}>View Profile</Dropdown.Item>
+          <Dropdown.Item href="/#">Orders</Dropdown.Item>
+          <Dropdown.Divider/>
+          <Dropdown.Item href="/#">Log Out</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
 };
 
-export default Profile;
+export default Profile
