44cf84e3b8c9cadbacb6f6af80eaac4f1e0e837c
Diogo Coelho <dcarvalh@student.42lisboa.com>
2022-12-14 23:49:35 +0000

    Added size when it matters

--- front-end/comp/OrdersPage/OrdersContainer/Order/Order.js
+++ front-end/comp/OrdersPage/OrdersContainer/Order/Order.js
@@ -1,8 +1,16 @@
 const Order = (props) => {
+    
+    const RenderSize = () => {
+        return (
+            <p>Size: {props.order.size}</p>
+        );
+    };
+    
     return(
         <div>
             <p>ID: {props.order.id}</p>
             <p>ITEM: {props.order.item}</p>
+            {props.order.size != "NULL"? RenderSize() : null}
             <p>Quantidade: {props.order.quantity}</p>
             <p>Preco total: {props.order.quantity * props.order.price}</p>
             <p>-------------------------</p>
