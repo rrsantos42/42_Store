336b77a30f0c3d0e04fa690c751677c9a3b3696d
Ricardo Rodrigues <ricardo.alexandre.rodrigues.42@gamil.com>
2022-12-28 15:47:54 +0000

    Start On OrdersPage

--- front-end/pages/Orders/index.js
+++ front-end/pages/Orders/index.js
@@ -1,10 +1,12 @@
-import Header from "../../comp/MainPage/Header/header";
+import Header from "../../comp/MainPage/Header/header"
+import OrdersPage from "../../comp/OrdersPage/OrdersPage";
 import styles from '../../styles/Backgroud.module.scss'
 const Orders = () =>{
 
     return(
         <div className={styles.mainbody}>
             <Header/>
+            <OrdersPage/>
         </div>
 
     );
